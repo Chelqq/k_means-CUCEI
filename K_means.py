891ff153f68829ed9177a3d93230f7e4bf2d26ef
XeL <cdezz8@gmail.com>
2022-11-02 14:59:16 -0600

    K_means program finished, clustering done with 6 centroids and 3 sample 2 test

--- K_means.py
+++ K_means.py
@@ -54,9 +54,66 @@ for i in range(1,20):
     ###For each K, we calculate the sum of total squared val in the Cluster
     inertia.append(algo.inertia_)
 
-#sum of total squared error
+#showing and knowing sum of total squared error
 plt.figure(figsize=[10,6])
 plt.title('Elbow method')
-plt.xlabel()
+plt.xlabel('Cluster No.')
+plt.ylabel('Inertia')
+plt.plot(list(range(1,20)), inertia, marker='x')
+plt.show()
 
+###Aplying clustering Algo##
+###We define the Algorithm with K val
+algo = KMeans(n_clusters=6, init='k-means++',
+              max_iter=400, n_init=10)
 
+##Tranning....
+algo.fit(X)
+
+##Centroid data is obtained, so labels
+centroids, labels = algo.cluster_centers_, algo.labels_
+
+#Using sample data to verify to wich cluster it belong
+sample_prediction = algo.predict(samples_escalated)
+for i, pred in enumerate(sample_prediction):
+    print("Sample", i, "Found in cluster:", pred)
+    
+
+### Show results next to data ###
+# Dimensionality reduction to data
+from sklearn.decomposition import PCA
+
+model_pca = PCA(n_components = 2)
+model_pca.fit(X)
+pca = model_pca.transform(X) 
+
+#We reduce dimensionality to centroids
+centroids_pca = model_pca.transform(centroids)
+
+#Pretty colors 4 each cluster
+colors = ['blue', 'red', 'green', 'orange', 'gray', 'brown']
+colors_cluster = [colors[labels[i]] for i in range(len(pca))]
+
+#ilustrating PCA components
+plt.scatter(pca[:, 0], pca[:, 1], c = colors_cluster, 
+            marker = 'o',alpha = 0.4)
+
+#Showing centroids
+plt.scatter(centroids_pca[:, 0], centroids_pca[:, 1],
+            marker = 'x', s = 100, linewidths = 3, c = colors)
+
+#keeping vars with easy names
+xvector = model_pca.components_[0] * max(pca[:,0])
+yvector = model_pca.components_[1] * max(pca[:,1])
+columns = data.columns
+
+#Ilustrating cluster names with vector distance
+for i in range(len(columns)):
+    #Showing Vectors
+    plt.arrow(0, 0, xvector[i], yvector[i], color = 'black', 
+              width = 0.0005, head_width = 0.02, alpha = 0.75)
+    #putting names
+    plt.text(xvector[i], yvector[i], list(columns)[i], color='black', 
+             alpha=0.75)
+
+plt.show()
